7c0d52408a75c1257923523c83514538701ac6e3
Ondřej Maxa <ondrej.maxa11@gmail.com>
2024-03-19 10:36:10 +0100

    feat: default configuration values

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # rust-backend
 
 A Rust GraphQL backend template repository
+
+Configuration:
+| **ENV** | **Description** | **Default** |
+|------------------|--------------------------------------------------------------|--------------------------|
+| PORT | HTTP port that the gateway will be exposed on. | 80 |
+| HEALTHCHECK_PORT | HTTP port that gateway healthcheck endpoint is available on. | 9000 |
+| CONFIG_FILE | Path to the configuration file | /etc/gateway/config.json |
+| REDIS_URL | Connection URL for redis database | redis://localhost:6379 |
--- src/env.rs
+++ src/env.rs
@@ -13,16 +13,12 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     InvalidPort(ParseIntError),
-    MissingConfigFile,
-    MissingRedisUrl,
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::InvalidPort(e) => write!(f, "Invalid port: {}", e),
-            Error::MissingConfigFile => write!(f, "CONFIG_FILE environment variable is missing"),
-            Error::MissingRedisUrl => write!(f, "REDIS_URL environment variable is missing"),
         }
     }
 }
@@ -38,8 +34,9 @@ impl Env {
                 .unwrap_or_else(|_| "9000".to_string())
                 .parse()
                 .map_err(Error::InvalidPort)?,
-            config: env::var("CONFIG_FILE").map_err(|_| Error::MissingConfigFile)?,
-            redis: env::var("REDIS_URL").map_err(|_| Error::MissingRedisUrl)?,
+            config: env::var("CONFIG_FILE")
+                .unwrap_or_else(|_| "/etc/gateway/config.json".to_string()),
+            redis: env::var("REDIS_URL").unwrap_or_else(|_| "redis://localhost:6379".to_string()),
         })
     }
 }
